e65c00bc40c753465cf633c169b3ce2f9492a47c
Erielcio Imamura <erielcio00@gmail.com>
2020-11-22 12:12:49 -0300

    update run_regressor

--- run_regressor.py
+++ run_regressor.py
@@ -1,12 +1,12 @@
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_log_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_log_error, r2_score
 import math
 
 from fema import FEMaRegressor
 
-
+# House Pricing
 data = datasets.load_boston()
 X = data.data
 y = data.target
@@ -20,6 +20,6 @@ regr.fit(X_train, y_train)
 y_pred = regr.predict(X_test)
 
 print(f'RMSE = {math.sqrt(mean_squared_error(y_test, y_pred))}')
-print(f'MSE = {mean_squared_error(y_test, y_pred)}')
 print(f'MAE = {mean_absolute_error(y_test, y_pred)}')
-print(f'RSLE = {mean_squared_log_error(y_test, y_pred)}')
+print(f'RMSLE = {math.sqrt(mean_squared_log_error(y_test, y_pred))}')
+print(f'R2 = {r2_score(y_test, y_pred)}')
